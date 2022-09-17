1096a50d34cd5003d87491f415e9b144de8893dc
Nachi4265 <99918049+Nachi4265@users.noreply.github.com>
2022-09-16 20:37:20 -0400

    images messed

--- Cookie.js
+++ Cookie.js
@@ -1,7 +1,25 @@
 var cookies = 0;
 
-function bakeCookies() {
-    cookies += 1;
-    document.getElementsByClassName('cookies').innerHTML = "counter" + cookies;
+var cpc = 1;
 
+
+function bakeCookies(){
+    cookies += cpc;
+    document.getElementById('cookie-text').innerHTML = " "+ cookies;
 }
+
+function double(){
+    cpc += 1;
+    document.getElementsByClassName('cookie-text').innerHTML = " "+ cookies;
+}
+
+function five(){
+    cpc += 4;
+    document.getElementsByClassName('cookie-text').innerHTML = " "+ cookies;
+}
+
+function craze(){
+    cpc += 100;
+    document.getElementsByClassName('cookie-text').innerHTML = " "+ cookies;
+}
+
